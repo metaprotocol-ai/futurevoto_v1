b8ab2c8fbeef821ef8febd2fc117c643a3d75959
METAPROTOCOL <app@metaprotocol.ai>
2022-06-08 17:27:39 +0900

    Update Urls

--- src/components/PhishingWarningBanner/index.tsx
+++ src/components/PhishingWarningBanner/index.tsx
@@ -43,8 +43,8 @@ const PhishingWarningBanner: React.FC = () => {
   const { t } = useTranslation()
   const [, hideBanner] = usePhishingBannerManager()
   const { isMobile, isMd } = useMatchBreakpoints()
-  const warningText = t("please make sure you're visiting https://pancakeswap.finance - check the URL carefully.")
-  const warningTextAsParts = warningText.split(/(https:\/\/pancakeswap.finance)/g)
+  const warningText = t("please make sure you're visiting https://future.voto - check the URL carefully.")
+  const warningTextAsParts = warningText.split(/(https:\/\/future.voto)/g)
   const warningTextComponent = (
     <>
       <Text as="span" color="warning" small bold textTransform="uppercase">
@@ -56,8 +56,8 @@ const PhishingWarningBanner: React.FC = () => {
           key={i}
           small
           as="span"
-          bold={text === 'https://pancakeswap.finance'}
-          color={text === 'https://pancakeswap.finance' ? '#FFFFFF' : '#BDC2C4'}
+          bold={text === 'https://future.voto'}
+          color={text === 'https://future.voto' ? '#FFFFFF' : '#BDC2C4'}
         >
           {text}
         </Text>
